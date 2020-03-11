fc62fe9d5d9a97d90fb98736a89cff71a531fa03
Kamal Maher <kmaher@mit.edu>
2020-03-11 14:22:46 -0400

    Made shell scripts executable

--- shell/circle.sh
+++ shell/circle.sh
@@ -4,5 +4,6 @@ date=$1
 
 # Loop through .avi files and submit processing jobs
 for vidfolder in /om2/user/$USER/data/circle_data_cluster/$date/*; do
-        sbatch ~/circle/shell/subcircle.sh $vidfolder
+        cp ~/circle/measure/1504_730_2304_2300.Ring.mat $vidfolder/$(basename $vidfolder).Ring.mat # copy generic ring to folder
+	sbatch ~/circle/shell/subcircle.sh $vidfolder
 done
