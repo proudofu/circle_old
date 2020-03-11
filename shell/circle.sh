25e758834a4bacd4b651e3cfa340bed40f9d5b07
Kamal Maher <kmaher@mit.edu>
2020-03-11 17:25:21 -0400

    Changed permissions on executable shell scripts

--- shell/circle.sh
+++ shell/circle.sh
@@ -5,5 +5,7 @@ date=$1
 # Loop through .avi files and submit processing jobs
 for vidfolder in /om2/user/$USER/data/circle_data_cluster/$date/*; do
         cp ~/circle/measure/1504_730_2304_2300.Ring.mat $vidfolder/$(basename $vidfolder).Ring.mat # copy generic ring to folder
-	sbatch ~/circle/shell/subcircle.sh $vidfolder
+	sbatch ~/circle/shell/subcircle.sh $date $(basename $vidfolder)
+	echo $date
+	echo $(basename $vidfolder)
 done
--- shell/subcircle.sh
+++ shell/subcircle.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #
-#SBATCH --output=/home/%u/log/subcircle_%j.txt
+#SBATCH --output=/home/%u/log/test_%j.txt
 #SBATCH -n1
-#SBATCH --job-name=subcircle_j%
+#SBATCH --job-name=test_%j
 #SBATCH --nodes=1
 #SBATCH --mem=40000
 #SBATCH --time=100:00:00
 
 # $1: Name of video
-vidfolder=$1
-date=${vidfolder%-*}
-echo $vidfolder
-echo $date
+date=$1
+vidname=$2
 
 # Load matlab and launch tracker command.
 module add mit/matlab/2014a
-matlab -nodisplay -r "addpath(genpath('/home/$USER/circle')), addpath(genpath('/om2/user/$USER/data/circle_data_cluster')), TrackerAutomatedScript2('/om2/user/$USER/data/circle_data_cluster/$date/$vidfolder/${vidfolder}.avi', 'scale', '/home/$USER/circle/measure/measureCam.avi', 'NumWorms', [30, 80]), exit;"
+matlab -nodisplay -r "disp('/om2/user/$USER/data/circle_data_cluster/$date/$vidname/${vidname}.avi'), addpath(genpath('/home/$USER/circle')), addpath(genpath('/om2/user/$USER/data/circle_data_cluster')), TrackerAutomatedScript2('/om2/user/$USER/data/circle_data_cluster/$date/$vidname/${vidname}.avi', 'scale', '/home/$USER/circle/measure/measureCam.avi', 'NumWorms', [30, 80]), exit;"
